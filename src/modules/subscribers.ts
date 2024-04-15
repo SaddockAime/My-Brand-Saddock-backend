21763f5ad6454b4e53b89001166543907de440a6
SaddockAime <aimegetz@gmail.com>
2024-04-15 20:29:06 +0200

    subscribers file

---
+++ src/modules/subscribers.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const subscriberSchema = new mongoose.Schema({
+    email: { type: String, required: true},
+});
+
+const Subscribers = mongoose.model('Subscribers', subscriberSchema);
+
+export default Subscribers;
