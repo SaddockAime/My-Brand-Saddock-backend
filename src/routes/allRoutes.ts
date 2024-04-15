aa69fce535b4fbe4ac8e1d6aa4ab27ea6088c0f7
SaddockAime <aimegetz@gmail.com>
2024-04-15 20:30:17 +0200

    Allroutes file

--- src/routes/allRoutes.ts
+++ src/routes/allRoutes.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 import userRoute from './userRoutes';
 import messageRoute from './messageRoutes';
+import subscriberRoute from './subscriberRoutes';
 
 const router = express.Router();
 
 router.use("/users", userRoute);
 router.use("/messages", messageRoute);
+router.use("/subscribers", subscriberRoute);
 
 export default router;
