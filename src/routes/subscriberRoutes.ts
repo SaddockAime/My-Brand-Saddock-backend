c66d1e70b45e470da3895787d6366d6e5f3662f4
SaddockAime <aimegetz@gmail.com>
2024-04-21 18:38:03 +0200

    subscriber routes

--- src/routes/subscriberRoutes.ts
+++ src/routes/subscriberRoutes.ts
@@ -1,9 +1,79 @@
 import express from "express";
-import { subscribers, viewSubscribers } from "../controllers/subscriberController";
+import { createSubscriber, viewSubscribers } from "../modules/subscriber/controller/subscriberController";
 
 const router = express.Router();
 
-router.post("/subscribers", subscribers);
+/**
+ * @swagger
+ * /subscribers/createSubscriber:
+ *   post:
+ *     summary: create a new subscriber
+ *     requestBody:
+ *       description: subscriber data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *           example:
+ *             email: "saddock@gmail.com"
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Subscription Sent"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     newSubscriber:
+ *                       type: object
+ *                       properties:
+ *                         _id:
+ *                           type: string
+ *                         email:
+ *                           type: string
+ *       500:
+ *         description: Internal Server Error
+ */
+router.post("/createSubscriber", createSubscriber);
+
+/**
+ * @swagger
+ * /subscribers/viewSubscribers:
+ *   get:
+ *     summary: Get a list of all subscribers
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     allSubscribers:
+ *                       type: object
+ *                       properties:
+ *                         _id:
+ *                           type: string
+ *                         email:
+ *                           type: string
+ *       404:
+ *         description: Subscribers were not found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get("/viewSubscribers", viewSubscribers);
 
 export default router;
