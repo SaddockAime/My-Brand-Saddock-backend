749ed2debfe95faaf747d7b15077fd4617b94b0c
Saddock Kabandana <aimegetz@gmail.com>
2024-05-07 13:50:34 +0200

    welcome message

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,12 @@ app.use(express.json());
 app.use(cors(" * " as any));
 dotenv.config();
 
+app.get('/', (rep: express.Request, res: express.Response)=>{
+    res.status(200).json({
+      message: 'Welcome to my Brand Backend API Saddock'
+    })
+  })
+
 app.use("/api", allRoutes);
 
 swaggerSetup(app);
