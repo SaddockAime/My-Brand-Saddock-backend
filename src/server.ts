644d9248d44286eda9aa5db60262ab1f9257cbe2
Saddock Kabandana <aimegetz@gmail.com>
2024-05-20 20:53:51 +0200

    updates

--- src/server.ts
+++ src/server.ts
@@ -21,9 +21,6 @@ app.use("/api", allRoutes);
 swaggerSetup(app);
 
 const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
 
 const startServer = async () => {
   try {
